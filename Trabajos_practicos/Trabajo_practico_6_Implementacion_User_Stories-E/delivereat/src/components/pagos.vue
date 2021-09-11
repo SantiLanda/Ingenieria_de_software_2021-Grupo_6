c1b3af83b19c9c27637c28f2070ee3b74cc0d1cc
Santiago Landa Valle <santi.land4@gmail.com>
2021-09-11 17:20:42 -0300

    Intento de ruteo a modificar

---
+++ Trabajos_practicos/Trabajo_practico_6_Implementacion_User_Stories-E/delivereat/src/components/pagos.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Pago: {{ carrito.id }}</div>
+</template>
+
+<script>
+export default {
+  mounted () {
+    console.log(this.$props)
+    console.log(this.carrito)
+  },
+  props: ['carrito']
+}
+</script>
--- Trabajos_practicos/Trabajo_practico_6_Implementacion_User_Stories-E/delivereat/src/components/seleccionar_carrito.vue
+++ Trabajos_practicos/Trabajo_practico_6_Implementacion_User_Stories-E/delivereat/src/components/seleccionar_carrito.vue
@@ -1,16 +1,47 @@
 <template>
-    <div class="cmb">
-      <label for="carrito"><b>Carrito: </b></label>
-      <select name="carrito" id="carrito" @input="updateValues()">
-        <option value="lleno">Carrito lleno</option>
-        <option value="vacio">Carrito vacio</option>
-      </select>
+  <div class="cmb">
+    <label for="carrito"><b>Carrito: </b></label>
+    <select name="carrito" id="carrito">
+      <option value="0">Carrito lleno</option>
+      <option value="1">Carrito vacio</option>
+    </select>
     <div>
-      <button href="/pago">Confirmar carrito</button>
+      <button @click="confirmarCarrito()">Confirmar carrito</button>
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
+// import $ from 'jquery'
 
+export default {
+  data: () => ({
+    carritos: [
+      { id: 1, items: [] },
+      {
+        id: 0,
+        items: [
+          { producto: 'Hamburguesa', precio: 500, cantidad: 2 },
+          { producto: 'Helado', precio: 100, cantidad: 4 },
+          { producto: 'Gaseosa', precio: 150, cantidad: 1 }
+        ]
+      }
+    ]
+  }),
+
+  methods: {
+    confirmarCarrito: function () {
+      this.$router.push({ name: 'Finalizar pedido', params: { carrito: this.selectCarrito() } })
+    },
+
+    selectCarrito: function () {
+      var idBusc = document.getElementById('carrito').value
+      for (var i = 0; i < this.carritos.length; i++) {
+        if (this.carritos[i].id === parseInt(idBusc)) {
+          return this.carritos[i]
+        }
+      }
+    }
+  }
+}
 </script>
