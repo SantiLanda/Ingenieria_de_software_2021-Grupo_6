9683516bf3a3a9d4ae8b4b3d66bf24b1c20fcf77
Santiago Landa Valle <santi.land4@gmail.com>
2021-09-11 01:34:32 -0300

    Inicio del proyecto y configuracion del main

---
+++ Trabajos_practicos/Trabajo_practico_6_Implementacion_User_Stories-E/delivereat/src/components/DeliverEatAppNavbar.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <div class="bar">
+      <img src="../../../Logo/DeliverEats.png" alt="DeliverEat Logo" style="width:15%;">
+    </div>
+    <div class="button-panel">
+      <button @click="showContact = !showContact">Contacto</button>
+    </div>
+    <div v-if="showContact">
+      <table class="contact-table">
+        <tr>
+          <th>Nombre</th>
+          <th>Legajo</th>
+          <th>Mail</th>
+        </tr>
+        <tr v-for="(contact, id) in data" :key="id">
+          <td>{{ contact.name }}</td>
+          <td>{{ contact.no }}</td>
+          <td>{{ contact.mail }}</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    showContact: false
+  }),
+  props: {
+    data: {
+      type: Array
+    }
+  },
+  name: 'delivereatnavbar'
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap');
+</style>
+
+<style scoped>
+  .contact-table {
+    width: 100%;
+    margin-bottom: 2rem;
+  }
+  .contact-table td {
+    color: white;
+    font-weight: bolder;
+    text-shadow: black 3px 3px;
+    font-size: x-large;
+    background-color: #7400B8;
+  }
+  .bar {
+    background-color: #7400B8;
+  }
+  .button-panel {
+    display: flex;
+    margin-left: 0.1rem;
+    margin-right: 0.1rem;
+  }
+  .button-panel button {
+    font-family: 'Oswald', sans-serif;
+    font-weight: bold;
+    border: 0;
+    margin: 0 5px;
+    background-color: #7400B8;
+    color: whitesmoke;
+    padding: 4px;
+    padding-left: 10px;
+    padding-right: 10px;
+    border-radius: 6px;
+    box-shadow: 5px 4px 5px 0px rgba(0,0,0,0.3);
+    -webkit-box-shadow: 5px 4px 5px 0px rgba(0,0,0,0.3);
+    -moz-box-shadow: 5px 4px 5px 0px rgba(0,0,0,0.3);
+    font-size: xx-large;
+    position: relative;
+    transition: all 0.1s;
+    top: -2rem;
+    text-shadow: black 3px 3px;
+  }
+  .button-panel button:hover {
+    top: -1rem;
+    background-color: #3577bd;
+  }
+</style>
--- Trabajos_practicos/Trabajo_practico_6_Implementacion_User_Stories-E/delivereat/src/main.js
+++ Trabajos_practicos/Trabajo_practico_6_Implementacion_User_Stories-E/delivereat/src/main.js
@@ -1,8 +1,73 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'jquery'
+import { DropdownPlugin, TablePlugin, CardPlugin, NavPlugin, NavbarPlugin, CarouselPlugin } from 'bootstrap-vue'
+import DeliverEatAppNavBar from './components/DeliverEatAppNavbar.vue';
+
+Vue.use(VueRouter)
+Vue.use(DropdownPlugin)
+Vue.use(TablePlugin)
+Vue.use(CardPlugin)
+Vue.use(NavPlugin)
+Vue.use(NavbarPlugin)
+Vue.use(CarouselPlugin)
 
 Vue.config.productionTip = false
 
+const routes = [
+  { path: '/',
+    name: 'Home',
+    component: () => import(/* webpackChunkName: "Home" */'./App.vue'),
+    meta: {
+      title: 'Inicio DeliverEat'
+    }
+  },
+
+  { path: '/carrito',
+    name: 'Confirmar carrito',
+    component: () => import(/* webpackChunkName: "Confirmar carrito" */'./pantallas/seleccionar_carrito.vue'),
+    meta: {
+      title: 'Confirmar compra'
+    }
+  },
+
+  { path: '/pago',
+    name: 'Datos de pago',
+    component: () => import(/* webpackChunkName: "Datos de pago" */'./pantallas/pago.vue'),
+    meta: {
+      title: 'Ingresar datos de pago'
+    }
+  },
+
+  { path: '/checkout',
+    name: 'Confirmacion pedido',
+    component: () => import(/* webpackChunkName: "Confirmacion pedido" */'./pantallas/confirmacion.vue'),
+    meta: {
+      title: 'Pedido realizado'
+    }
+  },
+
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: routes
+})
+
+// eslint-disable-next-line
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title || 'DeliverEat!';
+  next();
+});
+
+Vue.component('DeliverEatNavBar', DeliverEatAppNavBar);
+
 new Vue({
+  el: '#app',
   render: h => h(App),
+  router
 }).$mount('#app')
