877e20e75a3959f788e67cefff9bd981eb9e7076
Joaquin Esteban Giron Pignol <girjoes@gmail.com>
2021-09-12 23:46:11 -0300

    Agregado boton Volver con su funcionalidad en seleccionar_carrito

--- Trabajos_practicos/Trabajo_practico_6_Implementacion_User_Stories-E/delivereat/src/components/seleccionar_carrito.vue
+++ Trabajos_practicos/Trabajo_practico_6_Implementacion_User_Stories-E/delivereat/src/components/seleccionar_carrito.vue
@@ -1,14 +1,20 @@
 <template>
+<body>
   <div class="cmb">
     <label for="carrito"><b>Carrito: </b></label>
     <select name="carrito" id="carrito">
       <option value="0">Carrito lleno</option>
       <option value="1">Carrito vacio</option>
     </select>
-    <div>
-      <button @click="confirmarCarrito()">Confirmar carrito</button>
-    </div>
+    <h1>
+      <br>
+    </h1>
   </div>
+    <div>
+      <button class="confirmarCarrito" @click="confirmarCarrito()">Confirmar carrito</button>
+      <button class="volverAHome" @click="volverAHome()">Volver</button>
+    </div>
+  </body>
 </template>
 
 <script>
@@ -41,7 +47,75 @@ export default {
           return this.carritos[i]
         }
       }
+    },
+    volverAHome: function () {
+      this.$router.push('/')
     }
   }
 }
 </script>
+
+<style scoped>
+.confirmarCarrito {
+    font-family: 'Oswald', sans-serif;
+    font-weight: bold;
+    font-size: 30px;
+    display: inline-block;
+    border: 0;
+    margin: 0 5px;
+    background-color: #7400B8;
+    color: whitesmoke;
+    padding: 4px;
+    padding-left: 10px;
+    padding-right: 10px;
+    border-radius: 6px;
+    box-shadow: 5px 4px 5px 0px rgba(0,0,0,0.3);
+    -webkit-box-shadow: 5px 4px 5px 0px rgba(0,0,0,0.3);
+    -moz-box-shadow: 5px 4px 5px 0px rgba(0,0,0,0.3);
+    font-size: xx-large;
+    position: relative;
+    transition: all 0.1s;
+    bottom: auto;
+    top: -2rem;
+    text-shadow: black 3px 3px;
+}
+.confirmarCarrito:hover {
+    top: -1rem;
+    background-color: #56CFE1;
+}
+.confirmarCarrito:active {
+    position:relative;
+    top:1px;
+}
+.volverAHome {
+    font-family: 'Oswald', sans-serif;
+    font-weight: bold;
+    font-size: 30px;
+    display: inline-block;
+    border: 0;
+    margin: 0 5px;
+    background-color: #7400B8;
+    color: whitesmoke;
+    padding: 4px;
+    padding-left: 10px;
+    padding-right: 10px;
+    border-radius: 6px;
+    box-shadow: 5px 4px 5px 0px rgba(0,0,0,0.3);
+    -webkit-box-shadow: 5px 4px 5px 0px rgba(0,0,0,0.3);
+    -moz-box-shadow: 5px 4px 5px 0px rgba(0,0,0,0.3);
+    font-size: xx-large;
+    position: relative;
+    transition: all 0.1s;
+    bottom: auto;
+    top: -2rem;
+    text-shadow: black 3px 3px;
+}
+.volverAHome:hover {
+    top: -1rem;
+    background-color: #c00c15;
+}
+.volverAHome:active {
+    position:relative;
+    top:1px;
+}
+</style>
